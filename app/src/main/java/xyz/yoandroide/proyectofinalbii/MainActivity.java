3ef06c928e2992a43dbbc84d2dda78720423f677
LeninYair <l.camacho@intsuperior.edu.ec>
2022-09-10 18:16:09 -0500

    login y firebase

--- app/src/main/java/xyz/yoandroide/proyectofinalbii/MainActivity.java
+++ app/src/main/java/xyz/yoandroide/proyectofinalbii/MainActivity.java
@@ -2,7 +2,11 @@ package xyz.yoandroide.proyectofinalbii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText usuario;
+        EditText clave;
+        Button login;
+        Button register;
+
+        usuario=(EditText) findViewById(R.id.eTxtUsuario);
+        clave=(EditText)  findViewById(R.id.eTxtClave);
+        login=(Button) findViewById(R.id.btnLogin);
+        register=(Button) findViewById(R.id.btnRegister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuario.getText().toString();
+                String contrasenia = clave.getText().toString();
+
+                if (user=="lenin" || contrasenia=="yair"){
+                    Intent intent=new Intent(MainActivity.this,menuPrincipal.class);
+                    startActivity(intent);
+                }else{
+                    Intent intent=new Intent(MainActivity.this,menuPrincipal.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/xyz/yoandroide/proyectofinalbii/menuPrincipal.java
@@ -0,0 +1,21 @@
+package xyz.yoandroide.proyectofinalbii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class menuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+
+
+    }
+}
